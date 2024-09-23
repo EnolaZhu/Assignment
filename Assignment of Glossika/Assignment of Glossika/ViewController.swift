03b946a14c9765117300bd00442a3588551dc746
EnolaZhu <enola.zhu@ijoyful.com>
2024-09-23 10:54:17 +0800

    [1.0.0] Init project

---
+++ Assignment of Glossika/Assignment of Glossika/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment of Glossika
+//
+//  Created by enola.zhu on 2024/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
